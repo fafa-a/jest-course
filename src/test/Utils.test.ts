615051bbfbdb08827fb2480cf6af22c03e65c797
Puech Fabien (CSgroup)~ <fabien.puech@csgroup.eu>
2023-03-02 11:02:25 +0100

    assertions and matchers

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,4 +1,4 @@
-import { toUpperCase } from "../app/Utils";
+import { getStringInfo, toUpperCase } from "../app/Utils";
 
 describe("Utils test suite", () => {
   it("should return uppercase of a valid string", () => {
@@ -12,5 +12,36 @@ describe("Utils test suite", () => {
 
     // Assert
     expect(actual).toBe(expected);
+  });
+
+  it("should return info for valid string", () => {
+    const actual = getStringInfo("My-String");
+
+    expect(actual.lowerCase).toBe("my-string");
+    expect(actual.extraInfo).toEqual({});
+
+    expect(actual.characters.length).toBe(9);
+    expect(actual.characters).toHaveLength(9);
+
+    expect(actual.characters).toEqual([
+      "M",
+      "y",
+      "-",
+      "S",
+      "t",
+      "r",
+      "i",
+      "n",
+      "g",
+    ]);
+    expect(actual.characters).toContain<string>("M");
+    expect(actual.characters).toEqual(
+      expect.arrayContaining(["t", "r", "i", "n", "g", "M", "y", "-", "S"])
+    );
+
+    expect(actual.extraInfo).not.toBe(undefined);
+    expect(actual.extraInfo).not.toBeUndefined();
+    // expect(actual.extraInfo).not.toBeDefined();
+    expect(actual.extraInfo).toBeTruthy();
   });
 });
