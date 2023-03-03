f384af4a7ad3198fd0f6a52dad3b7141efbfadff
Puech Fabien (CSgroup)~ <fabien.puech@csgroup.eu>
2023-03-03 11:03:26 +0100

    mocks

--- src/test/doubles/OtherUtils.spec.ts
+++ src/test/doubles/OtherUtils.spec.ts
@@ -4,6 +4,56 @@ import {
 } from "../../app/doubles/OtherUtils";
 
 describe("OtherUtils", () => {
+  describe.only("Tracking callbacks with Jest mocks", () => {
+    const callBackmock = jest.fn();
+    afterEach(() => {
+      callBackmock.mockClear();
+      // jest.clearAllMocks();
+    });
+
+    it("Calls callback for invalid argument - track calls", () => {
+      const actual = toUpperCaseWithCb("", callBackmock);
+      expect(actual).toBeUndefined();
+      expect(callBackmock).toBeCalledWith("Invalid argument");
+      expect(callBackmock).toBeCalledTimes(1);
+    });
+
+    it("Calls callback for valid argument - track calls", () => {
+      const actual = toUpperCaseWithCb("abc", callBackmock);
+      expect(actual).toBe("ABC");
+      expect(callBackmock).toBeCalledWith("Called function with abc");
+      expect(callBackmock).toBeCalledTimes(1);
+    });
+  });
+  describe("Tracking callbacks", () => {
+    let cbArgs = [];
+    let timesCalled = 0;
+
+    function callBackmock(arg: string) {
+      cbArgs.push(arg);
+      timesCalled++;
+    }
+
+    afterEach(() => {
+      cbArgs = [];
+      timesCalled = 0;
+    });
+
+    it("Calls callback for invalid argument - track calls", () => {
+      const actual = toUpperCaseWithCb("", callBackmock);
+      expect(actual).toBeUndefined();
+      expect(cbArgs).toContain("Invalid argument");
+      expect(timesCalled).toBe(1);
+    });
+
+    it("Calls callback for valid argument - track calls", () => {
+      const actual = toUpperCaseWithCb("abc", callBackmock);
+      expect(actual).toBe("ABC");
+      expect(cbArgs).toContain("Called function with abc");
+      expect(timesCalled).toBe(1);
+    });
+  });
+
   it("toUpperCaseWithCb - calls callback for invalid argument", () => {
     const actual = toUpperCaseWithCb("", () => {});
     expect(actual).toBeUndefined();
