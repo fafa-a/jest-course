e04078c39425e144eecf283b5d0b82f121998ecc
Puech Fabien (CSgroup)~ <fabien.puech@csgroup.eu>
2023-03-03 10:41:16 +0100

    fake useless

--- src/app/doubles/OtherUtils.ts
+++ src/app/doubles/OtherUtils.ts
@@ -6,6 +6,18 @@ export type stringInfo = {
   extraInfo: Object | undefined;
 };
 
+type LoggerService = (arg: string) => void;
+
 export function calculateComplexity(stringInfo: stringInfo) {
   return Object.keys(stringInfo.extraInfo).length * stringInfo.length;
+}
+
+export function toUpperCaseWithCb(arg: string, callback: LoggerService) {
+  if (!arg) {
+    callback("Invalid argument");
+    return;
+  }
+  callback(`Called function with ${arg}`);
+
+  return arg.toUpperCase();
 }
--- src/test/doubles/OtherUtils.spec.ts
+++ src/test/doubles/OtherUtils.spec.ts
@@ -1,6 +1,19 @@
-import { calculateComplexity } from "../../app/doubles/OtherUtils";
+import {
+  calculateComplexity,
+  toUpperCaseWithCb,
+} from "../../app/doubles/OtherUtils";
 
 describe("OtherUtils", () => {
+  it("toUpperCaseWithCb - calls callback for invalid argument", () => {
+    const actual = toUpperCaseWithCb("", () => {});
+    expect(actual).toBeUndefined();
+  });
+
+  it("toUpperCaseWithCb - calls callback for valid argument", () => {
+    const actual = toUpperCaseWithCb("abc", () => {});
+    expect(actual).toBe("ABC");
+  });
+
   it("Calculate complexity", () => {
     const someInfo = {
       length: 5,
